6c72b339987b2c2b965f1aa2df030ca2ff9cba62
nomadqwerty <davexmike72@gmail.com>
2024-01-18 08:47:07 +0100

    Math:numberline, equalities and strict equalities

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+// import libs
+// IO Stream, input outPut lib: Gives compiler ability to output data onto the terminal, and recieve data pass into terminal; cli based
 #include <iostream>
 
-using namespace std;
-
+// main function: This serves like the global scope for a c++ program: The main execution context where all code is written and executed.
 int main(){
-    int number = 0;
-    cout << "hello world from 1" << endl;
+
+    // std keyword: ?
+
+    // variables
+    int favNum;
+    // cout for outputting data onto the terminal
+    std::cout << "Enter a number";
+
+    // cin for receiving data pass into terminal
+    // >> favNum: value received from terminal input is passed into favNum variable
+    std::cin >> favNum;
+
+    // std::endl is an instruction the tells the terminal to move to the next line and ends execution.
+     std::cout << "This is what I received";
+     std::cout << favNum << std::endl;
+
+    // return ends function;
     return 0;
 }
--- pymaxfn/src/app/page.js
+++ pymaxfn/src/app/page.js
@@ -1,95 +1,147 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import JSZip from "jszip";
 import CryptoJS from "crypto-js";
 
+let downloadFile = (file, fileName, ext) => {
+  let elem = window.document.createElement("a");
+  elem.href = window.URL.createObjectURL(file);
+  elem.download = `${fileName}.${ext}`;
+  elem.click();
+};
+
 export default function Home() {
+  // state
   let [file, setFile] = useState(null);
+  let [fileName, setFileName] = useState("file");
+  let [testKey, setTestKey] = useState("12345678");
   let [zipFile, setZipFile] = useState(null);
   let [encFile, setEncFile] = useState(null);
+  let [decFile, setDecFile] = useState(null);
 
-  const zip = new JSZip();
-
+  // filter Object
   const acceptedTypes = {
     "text/plain": true,
     "application/pdf": true,
     "image/jpeg": true,
-  };
-  let testKey = "12345678";
-
-  let downloadFile = (file, ext) => {
-    let elem = window.document.createElement("a");
-    elem.href = window.URL.createObjectURL(file);
-    elem.download = `file.${ext}`;
-    elem.click();
+    "image/png": true,
+    size: 1500000,
   };
 
-  function convertWordArrayToUint8Array(wordArray) {
-    var arrayOfWords = wordArray.hasOwnProperty("words") ? wordArray.words : [];
-    var length = wordArray.hasOwnProperty("sigBytes")
-      ? wordArray.sigBytes
-      : arrayOfWords.length * 4;
-    var uInt8Array = new Uint8Array(length),
-      index = 0,
-      word,
-      i;
-    for (i = 0; i < length; i++) {
-      word = arrayOfWords[i];
-      uInt8Array[index++] = word >> 24;
-      uInt8Array[index++] = (word >> 16) & 0xff;
-      uInt8Array[index++] = (word >> 8) & 0xff;
-      uInt8Array[index++] = word & 0xff;
-    }
-    return uInt8Array;
-  }
+  const zip = new JSZip();
 
+  // helper functions:
   let compressFile = async (uploadedFile) => {
     zip.file(uploadedFile.name, uploadedFile);
-
     const blobData = await zip.generateAsync({ type: "blob" });
-    let zipBlob = new Blob([blobData]);
+    setZipFile(blobData);
+  };
 
+  const encryptFile = (zipBlob, testKey) => {
     // encrypt
     let reader = new FileReader();
-    let fileEnc;
-    reader.onload = () => {
-      console.log("reader on");
-      let wordArray = CryptoJS.lib.WordArray.create(reader.result);
 
+    reader.onload = () => {
+      let wordArray = CryptoJS.lib.WordArray.create(reader.result);
       let encrypted = CryptoJS.AES.encrypt(wordArray, testKey);
 
-      fileEnc = new Blob([encrypted]);
-      downloadFile(fileEnc, "enc");
-
-      // decrypt
-      let decReader = new FileReader();
-      decReader.onload = () => {
-        console.log("dec reader on");
-        let decrypted = CryptoJS.AES.decrypt(encrypted, testKey);
-
-        let typedArr = convertWordArrayToUint8Array(decrypted);
-
-        let fileDec = new Blob([typedArr]);
-        downloadFile(fileDec, "zip");
-      };
-      decReader.readAsText(fileEnc);
+      setEncFile(encrypted);
     };
 
     reader.readAsArrayBuffer(zipBlob);
-    return fileEnc;
   };
 
+  const decryptFile = (encrypted, testKey) => {
+    // convert wordArray to 8bit array
+    function convertWordArrayToUint8Array(wordArray) {
+      var arrayOfWords = wordArray.hasOwnProperty("words")
+        ? wordArray.words
+        : [];
+      var length = wordArray.hasOwnProperty("sigBytes")
+        ? wordArray.sigBytes
+        : arrayOfWords.length * 4;
+      var uInt8Array = new Uint8Array(length),
+        index = 0,
+        word,
+        i;
+      for (i = 0; i < length; i++) {
+        word = arrayOfWords[i];
+        uInt8Array[index++] = word >> 24;
+        uInt8Array[index++] = (word >> 16) & 0xff;
+        uInt8Array[index++] = (word >> 8) & 0xff;
+        uInt8Array[index++] = word & 0xff;
+      }
+      return uInt8Array;
+    }
+
+    let decReader = new FileReader();
+    decReader.onload = () => {
+      let decrypted = CryptoJS.AES.decrypt(encrypted, testKey);
+      let typedArr = convertWordArrayToUint8Array(decrypted);
+
+      setDecFile(typedArr);
+    };
+    decReader.readAsText(new Blob([encrypted]));
+  };
+
+  // compress file
+  useEffect(() => {
+    (async () => {
+      if (file) {
+        console.log("compressing file...");
+        await compressFile(file);
+      }
+    })();
+  }, [file]);
+
+  // encryptFile
+  useEffect(() => {
+    (async () => {
+      if (zipFile) {
+        console.log("encrypting file...");
+        await encryptFile(zipFile, testKey);
+      }
+    })();
+  }, [zipFile]);
+
+  // decrypt file
+  useEffect(() => {
+    (async () => {
+      if (encFile) {
+        console.log("decrypting file...");
+        await decryptFile(encFile, testKey);
+      }
+    })();
+  }, [encFile]);
+
+  useEffect(() => {
+    (async () => {
+      if (decFile) {
+        console.log("downloading file");
+        downloadFile(new Blob([encFile]), fileName, "enc");
+        downloadFile(new Blob([decFile]), fileName, "zip");
+      }
+    })();
+  }, [decFile]);
+
+  // input handler
   const onFileInput = async (e) => {
     const uploadedFile = e.target.files[0];
     const type = uploadedFile.type;
+    const isTooBig = uploadedFile.size > acceptedTypes.size;
+    console.log("size: ", uploadedFile.size);
+    console.log("type: ", type);
     const isAllowed = acceptedTypes[type];
-    if (isAllowed) {
+    if (isAllowed && !isTooBig) {
+      let name = uploadedFile.name.split(".")[0];
+
       setFile(uploadedFile);
-      let compressed = await compressFile(uploadedFile);
+      setFileName(name);
     }
   };
 
+  // input jsx
   return (
     <div>
       <input type="file" id="file-selector" multiple onChange={onFileInput} />
